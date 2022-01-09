f1034e437016e8b2e36084e3322cc65c14454868
Biplob Sutradhar <biplobsd11@gmail.com>
2022-01-09 21:33:06 +0600

    Hello world

---
+++ helloword/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text("Hello World."));
+  }
+}
